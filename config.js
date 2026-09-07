479d8ffcda2cc80428bc77a2e4c0b463371f4f80
giovannipiccionebusiness-sudo <giovanni.piccione.business@gmail.com>
2026-09-07 09:59:09 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   // Incolla qui l'URL /exec della Web App Google Apps Script dopo il deploy.
-  API_URL: "PASTE_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE"
+  API_URL: "https://script.google.com/macros/s/AKfycbx028rqO9xMxngp53m_MIhPryxFqoncOwc01hUSIYAdXous5QFIY2_jfdG7IWRZ29S1/exec"
 };
